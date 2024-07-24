d914bc06f48558cdaac9581166d1dada91f99f67
Chiranchievi <chiranchievis2003@gmail.com>
2024-07-24 23:23:31 +0530

    Frontend design Changed

--- Heart_attack.py
+++ Heart_attack.py
@@ -1,6 +1,8 @@
 from flask import Flask, render_template, request
 import pickle
 
+global prediction
+
 app = Flask(__name__)
 classifier = pickle.load(open('classifier.pkl', 'rb'))
 
@@ -19,11 +21,16 @@ def home():
         prediction = classifier.predict([features])
         if prediction == [1]:
             value = 'The Person have Heart Attack'
-        else:
+            return render_template('Positiveresult.html', prediction=value)
+        elif prediction == [0]:
             value = 'The Person does not have Heart Attack'
-        return render_template('index.html', prediction = value)
+            return render_template('Negativeresult.html', prediction = value)  
 
     return render_template('index.html', prediction = None)
+
+@app.route('/Details')
+def Details():
+    return render_template('Details.html')
 
 if __name__ == "__main__":
     app.run(host='localhost', port=8000, debug=True)
